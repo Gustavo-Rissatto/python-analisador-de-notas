ad696b6a56c90500a37ca454a1c7f13c7f9678dd
Gustavo Rissatto <grissatto@Lenovo.com>
2026-07-30 11:46:53 -0300

    Atualização do code

--- diagnostico.py
+++ diagnostico.py
@@ -1,11 +1,21 @@
-nome = input ("Digite o nome do aluno:")
-nota01 = float(input ("Digite a nota 01:"))
-nota02 = float(input ("Digite a nota 02:"))
-nota03 = float(input ("Digite a nota 03:"))
+def calcular_media(nota1, nota2, nota3):
+    media = (nota1 + nota2 + nota3) / 3
+    return media
 
-print ()
-print ("Nome do aluno:",nome)
-print ("nota 01:", nota01)
-print ("nota 02:", nota02)
-print ("nota 03:", nota03)
-print ("Media da nota", (nota01+nota02+nota03)/3)
+
+def verificar_situacao(media):
+    if media >= 7:
+        return "Aprovado"
+    else:
+        return "Reprovado"
+
+
+nome = input("Digite o nome do aluno: ")
+nota01 = float(input("Digite a nota 01: "))
+nota02 = float(input("Digite a nota 02: "))
+nota03 = float(input("Digite a nota 03: "))
+
+media = calcular_media(nota01, nota02, nota03)
+situacao = verificar_situacao(media)
+
+print(f"O aluno {nome} teve média {media:.2f}")
