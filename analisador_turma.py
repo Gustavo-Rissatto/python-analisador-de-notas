3d123d7d477bcf359daaec452b2ed69b5d0ffb66
Assistente Manha <amanha@Lenovo.com>
2026-08-11 07:48:29 -0300

    Atualizado

--- analisador_turma.py
+++ analisador_turma.py
@@ -3,26 +3,26 @@ print("     ANALISADOR DE TURMA")
 print("=================================")
 
 
-while input("FIM"):
-    nome = input("Digite o nome do aluno: ")
-    print("Aluno:", nome)
-    nota1 = float(input("Digite a nota do primeiro trimestre: "))
-    nota2 = float(input("Digite a nota do segundo trimestre: "))
-    nota3 = float(input("Digite a nota do terceiro trimestre: "))
 
-    media = (nota1+nota2+nota3)/3
+nome = input("Digite o nome do aluno: ")
+print("Aluno:", nome)
+nota1 = float(input("Digite a nota do primeiro trimestre: "))
+nota2 = float(input("Digite a nota do segundo trimestre: "))
+nota3 = float(input("Digite a nota do terceiro trimestre: "))
 
-    print("O nome do aluno: ", nome)
-    print("Nota primerio trimestre: ", nota1)
-    print("Nota segundo trimestre: ", nota2)
-    print("Nota terceiro trimestre: ", nota3)
-    print("Média: {:.2f}".format(media))
+media = (nota1+nota2+nota3)/3
+
+print("O nome do aluno: ", nome)
+print("Nota primerio trimestre: ", nota1)
+print("Nota segundo trimestre: ", nota2)
+print("Nota terceiro trimestre: ", nota3)
+print("Média: {:.2f}".format(media))
 
 
-    if media >= 7.0:
-        print("Aprovado")
-    else:
-        print("Reprovado")
+if media >= 7.0:
+    print("Aprovado")
+else:
+    print("Reprovado")
 
 
 
